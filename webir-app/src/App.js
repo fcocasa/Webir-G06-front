a7676c8e80beaaa05ee3d0319f6c60f15663eafd
M-Borba <m.borba98@hotmail.com>
2021-10-21 19:30:05 -0300

    fetch con sku, mejorar a link en futuro

--- webir-app/src/App.js
+++ webir-app/src/App.js
@@ -1,20 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [sku, setSku] = useState("");
+  const fetchProduct = (mlu) => {
+    fetch("https://api.mercadolibre.com/items/MLU" + mlu)
+      .then((res) => {
+        res
+          .json()
+          .then((json) => console.log(json))
+          .catch((error) =>
+            console.error("Error parsing json from meli:", error)
+          );
+      })
+      .catch((error) => console.error("Error fetching from meli:", error));
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          CAMEL CAMEL CAMEL para MercadoLibre
-        </p>
-        <p>
-        Ingrese sku del producto a notificar:
-        </p>
-        <div style={{display: 'flex',alignItems: 'center',gap: '1vh'}}>
-        <input type="text" style={{height: '2vh' ,width: '20vw'}}/> <button > Buscar </button>
-        <br/>
+        <p>CAMEL CAMEL CAMEL para MercadoLibre</p>
+        <p>Ingrese sku del producto a notificar:</p>
+        <div style={{ display: "flex", alignItems: "center", gap: "1vh" }}>
+          <input
+            type="text"
+            style={{ height: "2vh", width: "20vw" }}
+            value={sku}
+            onChange={(e) => setSku(e.target.value)}
+          />
+          <button onClick={() => fetchProduct(sku)}> Buscar </button>
+          <br />
         </div>
         <a
           className="App-link"
